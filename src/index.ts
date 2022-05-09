be9f12b6657976555a9f05c6631bc662f390410b
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2022-05-08 23:32:16 -0500

    Added Initial Source

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import express from "express";
+import makeWASocket, { DisconnectReason, useSingleFileAuthState } from "@adiwajshing/baileys";
+import { Boom } from "@hapi/boom";
+
+const app = express();
+const { state, saveState } = useSingleFileAuthState("./auth.json");
+async function startBot() {
+   const socket = makeWASocket({
+    auth: state,
+    printQRInTerminal: true,
+  });
+
+  // Make connection
+  socket.ev.on("connection.update", (update) => {
+    const { connection, lastDisconnect, qr } = update;
+    if (connection === "close") {
+      const shouldReconnect =
+        (lastDisconnect!.error as Boom)?.output?.statusCode !==
+        DisconnectReason.loggedOut;
+      console.log(
+        "connection closed due to error ",
+        // lastDisconnect!.error,
+        ", reconnecting ",
+        shouldReconnect
+      );
+      if (shouldReconnect) {
+        startBot();
+      }
+    } else if (connection === "open") {
+      console.log("Connection opened");
+    }
+  });
+
+  socket.ev.on("creds.update", saveState);
+
+  socket.ev.on("messages.upsert", ({ messages }) => {
+    const remitente = messages[0].key.remoteJid;
+    const texto = messages[0].message?.conversation;
+  });
+  app.get("/", async (req, res) => {
+    res.send("Hello World!");
+    await socket.sendMessage("573135408570@s.whatsapp.net", {
+      text: "Hello there!",
+    });
+  });
+}
+
+// run in main file
+try {
+  startBot(); 
+} catch (error) {
+  console.log("BOT CRASHED")
+  startBot();
+}
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
